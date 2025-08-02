81015f83c27a7e503c561b0709c2fa92f6be5925
fanda-blazek <blazek127@gmail.com>
2025-08-02 16:02:11 +0200

    add another example so we can work with it more easily

--- src/examples.ts
+++ src/examples.ts
@@ -1,4 +1,8 @@
-import { nestedDialogExample } from "./raw/dialog/dialog-examples";
+import {
+  edgeScrollbarDialogExample,
+  basicDialogExample,
+  nestedDialogExample,
+} from "./raw/dialog/dialog-examples";
 
 document.querySelector<HTMLDivElement>("#examples")!.innerHTML = `
   <div class="max-w-5xl text-center mx-auto flex flex-col items-center justify-center mt-12 gap-4 p-8 rounded-xl bg-zinc-100 dark:bg-zinc-900">
@@ -6,7 +10,13 @@ document.querySelector<HTMLDivElement>("#examples")!.innerHTML = `
 
     <div class="mt-12 flex flex-col items-center justify-center gap-5">
       <div>
+        ${basicDialogExample}
+      </div>
+      <div>
         ${nestedDialogExample}
+      </div>
+      <div>
+        ${edgeScrollbarDialogExample}
       </div>
     </div>
   </div>
